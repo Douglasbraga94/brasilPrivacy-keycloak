c7a06d9aa22216d5ab14469de1b902ec9ac15d13
Douglas Braga <douglasbraga@Douglass-Mac-mini.local>
2025-08-05 15:20:41 -0300

    Debug: imprime variáveis de ambiente e força hostname no start do Keycloak

--- keycloak-imports/keycloak-import.sh
+++ keycloak-imports/keycloak-import.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
 
-# Inicia o Keycloak em modo produção e importa o realm se existir, com logs detalhados
-exec /opt/keycloak/bin/kc.sh start --import-realm --verbose
+# Debug: imprime variáveis de ambiente
+echo "KC_HOSTNAME=$KC_HOSTNAME"
+echo "KC_HOSTNAME_STRICT=$KC_HOSTNAME_STRICT"
+echo "KC_HOSTNAME_URL=$KC_HOSTNAME_URL"
+
+# Inicia o Keycloak em modo produção, passando o hostname explicitamente
+exec /opt/keycloak/bin/kc.sh start --import-realm --hostname=$KC_HOSTNAME --verbose
