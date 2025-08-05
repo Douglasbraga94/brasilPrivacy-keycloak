b8f0d585f1b6f3a2d5adeb3c9ce4b7e2cb14075c
Douglas Braga <douglasbraga@Douglass-Mac-mini.local>
2025-08-05 15:09:12 -0300

    Adiciona --verbose ao script de inicialização do Keycloak para logs detalhados

--- keycloak-imports/keycloak-import.sh
+++ keycloak-imports/keycloak-import.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-# Inicia o Keycloak em modo produção e importa o realm se existir
-exec /opt/keycloak/bin/kc.sh start --import-realm
+# Inicia o Keycloak em modo produção e importa o realm se existir, com logs detalhados
+exec /opt/keycloak/bin/kc.sh start --import-realm --verbose
